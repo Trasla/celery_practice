165173da322efa2427207c7fe007aa14ba41c3c1
OSCAR IVAN TORRES TRASLAVIÑA <otorres@lsv-tech.com>
2019-06-26 13:43:57 -0500

    Rabbitmq practica

--- emit_log.py
+++ emit_log.py
@@ -1,9 +1,17 @@
 import pika
-
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+import sys
+import json
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
+
 channel.exchange_declare(exchange='logs', exchange_type='fanout')
-message = 'Hello World!'
+if len(sys.argv) > 2:
+    type_error = (sys.argv[1]).lower()
+    body = ' '.join(sys.argv[2:])
+    message = json.dumps({'type': type_error, 'body': body})
+else:
+    message = json.dumps({'type': 'info', 'body': 'hello'})
 channel.basic_publish(exchange='logs', routing_key='', body=message)
-print('[x] sent')
+print(" [x] Sent %r" % message)
 connection.close()
--- receive_log.py
+++ receive_log.py
@@ -1,17 +1,77 @@
+import json
 import pika
+import smtplib
+import ssl
+import sys
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
+
 channel.exchange_declare(exchange='logs', exchange_type='fanout')
-result_queue = channel.queue_declare(queue='', exclusive=True)
-queue_name = result_queue.method.queue
-channel.queue_bin(exchange='logs', queue=queue_name)
+
+result = channel.queue_declare(queue='', exclusive=True)
+queue_name = result.method.queue
+
+channel.queue_bind(exchange='logs', queue=queue_name)
+listen_error = sys.argv[1].lower()
+errors = ['debug', 'info', 'warning', 'error']
+print(' [*] Waiting for logs. To exit press CTRL+C'+listen_error)
 
 
 def callback(ch, method, properties, body):
-    print(f'[x] received {body}')
+    total = json.loads(body)
+    type_error = total['type'].replace("'", "").lower()
+    body_message = total['body']
+    asert = type_error in errors and listen_error == "debug"
+    print(asert)
+    if listen_error == "debug" and type_error in errors:
+        file_write(type_error, body_message)
+        emails(type_error, body_message)
+        print(type_error)
+        print(" [x] %r" % str(total['type']))
+    elif listen_error == "info":
+        file_write(type_error, body_message)
+        emails(type_error, body_message)
+        print(type_error)
+        print(" [x] %r" % str(total['type']))
+    elif listen_error == "warning":
+        file_write(type_error, body_message)
+        emails(type_error, body_message)
+        print(type_error)
+        print(" [x] %r" % str(total['type']))
+    elif listen_error == "error" and type_error == listen_error:
+        file_write(type_error, body_message)
+        emails(type_error, body_message)
+        print(type_error)
+        print(" [x] %r" % str(total['type']))
 
 
-channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
-print('Starting consuming...')
-channel.start_consumming()
+channel.basic_consume(
+    queue=queue_name, on_message_callback=callback, auto_ack=True)
+
+channel.start_consuming()
+
+
+def file_write(tip, total):
+    f = open(tip + '.txt', '+a')
+    f.write('Log = ' + tip + ': ' + total + '\n')
+    f.close()
+
+
+def emails(tip, total):
+    port = 465  # For SSL
+    smtp_server = "smtp.gmail.com"
+    sender_email = os.getenv("SENDER_EMAIL")
+    receiver_email = os.getenv("EMAIL_RECEIVER")
+    password = os.getenv("PASS_EMAIL")
+    message = 'Log = ' + tip + ': ' + total + '\n'
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
